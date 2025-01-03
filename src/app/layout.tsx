1ab2c3d011c507c2e77634b725aaa4f130a05f01
ZenoCoding <tychoyoung@gmail.com>
2025-01-02 19:48:58 -0800

    nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,18 @@
 import type {Metadata} from "next";
 import "./globals.css";
+import { Roboto } from "next/font/google";
 import Navbar from "@/components/Nav";
 import React from "react";
 import Footer from "@/components/Footer";
 import Head from "next/head";
+
+const roboto = Roboto(
+    {
+        subsets: ["latin"],
+        weight: ['100', '300', '400', '500', '700'],
+        display: "swap",
+    }
+);
 
 export const metadata: Metadata = {
     title: "STUCK Tutoring | Fremont CA",
@@ -16,7 +25,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={roboto.className}>
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './Nav.module.css';
+import Image from "next/image";
 
 const Nav: React.FC = () => {
     return (
-        <nav>
-            <ul className={styles.navList}>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/mission">Mission nuts</Link></li>
-            </ul>
+        <nav className={"h-16 w-full"}>
+            <div className={"fixed top-0 left-0 w-full bg-amber-400 drop-shadow-xl z-50"}>
+                <div className={"max-w-7xl mx-auto px-20"}>
+                    <div className={"flex items-center justify-between h-16"}>
+                        <Link href={"/"} className={"flex items-center"}>
+                            <Image src={"/logo.png"} width={50} height={50} alt={"logo"}></Image>
+                            <h1 className={"font-bold text-2xl ml-4"}>STUCK</h1>
+                        </Link>
+                        <div className="flex items-end space-x-8">
+                            <Link href="/about" className="font-semibold">ABOUT</Link>
+                            <Link href="/news" className="font-semibold">NEWS</Link>
+                            <Link href="/tutors" className="font-semibold">TUTORS</Link>
+                            <Link href="/programs" className="font-semibold">PROGRAMS</Link>
+                            <Link href="/sponsors" className="font-semibold">SPONSORS</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 };
